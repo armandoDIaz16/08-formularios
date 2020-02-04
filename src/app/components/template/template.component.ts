998963f7124e2fc061ddca03474548cef81d4e35
JOSE ARMANDO <jose.armando.diaz.97@gmail.com>
2020-02-04 17:56:04 -0600

    se llego a la clase 180 0 190 jaja

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,19 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Usuario } from '../../models/usuario.model';
+
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: []
+  styleUrls: ['../../app.component.css']
 })
 export class TemplateComponent implements OnInit {
+
+  usuario: Usuario = {
+    nombre: null,
+    apellido: null,
+    correo: null,
+  };
 
   constructor() { }
 
   ngOnInit() {
   }
 
-   guardar() {
+   guardar(forma: NgForm) {
      console.log("Se ha enviado el formulario");
+     console.log(forma);
+     console.log(forma.value);
     
   }
 
